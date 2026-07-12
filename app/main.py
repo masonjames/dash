agno-agi/data-agent

feat(phase3): add ops warehouse schema, Ops Dash agent, and knowledge base
Phase 3.1-3.2 of the Unified Platform Capsule Roadmap:

- Add ops_warehouse.sql migration with 8 tables: desired_services,
  actual_services, drift_observations, deploy_events, docker_events,
  incident_markers, update_status, state_snapshots
- Add agents_ops.py — Ops-flavored Dash variant with separate knowledge
  base, ops-specific SQLTools connection, and operational instructions
- Add 8 semantic table JSONs for the ops warehouse knowledge layer
- Add ops_metrics.json with drift debt, deploy success rate, incident
  frequency, exposure multiplier business rules and gotchas
- Add ops_queries.sql with 10 seed validated queries (drift ledger,
  version triangulation, crash loop detection, platform health score, etc.)
- Register Ops Dash + Reasoning Ops Dash in AgentOS app

Co-Authored-By: Claude Opus 4.6 <noreply@anthropic.com>

app/main.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from agno.os import AgentOS
 
 from dash.agents import dash, dash_knowledge, reasoning_dash
+from dash.agents_ops import ops_dash, ops_knowledge, reasoning_ops_dash
 from db import get_postgres_db
 
 # ============================================================================
@@ -23,8 +24,8 @@ agent_os = AgentOS(
     name="Dash",
     tracing=True,
     db=get_postgres_db(),
-    agents=[dash, reasoning_dash],
-    knowledge=[dash_knowledge],
+    agents=[dash, reasoning_dash, ops_dash, reasoning_ops_dash],
+    knowledge=[dash_knowledge, ops_knowledge],
     config=str(Path(__file__).parent / "config.yaml"),
 )
 

masonjames/dash

app/main.py
@@ -16,6 +16,7 @@ from agno.os import AgentOS
 
 from dash.agents.analyst import analyst
 from dash.agents.engineer import engineer
+from dash.agents_ops import ops_dash, ops_knowledge, reasoning_ops_dash
 from dash.settings import SLACK_SIGNING_SECRET, SLACK_TOKEN, dash_knowledge, dash_learnings
 from dash.team import dash
 from db import get_postgres_db
@@ -81,8 +82,8 @@ agent_os = AgentOS(
     lifespan=lifespan,
     db=get_postgres_db(),
     teams=[dash],
-    agents=[analyst, engineer],
-    knowledge=[dash_knowledge, dash_learnings],
+    agents=[analyst, engineer, ops_dash, reasoning_ops_dash],
+    knowledge=[dash_knowledge, dash_learnings, ops_knowledge],
     interfaces=interfaces,
     config=str(Path(__file__).parent / "config.yaml"),
 )